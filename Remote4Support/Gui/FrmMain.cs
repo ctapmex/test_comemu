4124a1450d98ca6442889605b9b5d981996ea499
Aleksey Dobrunov <Aleksey.Dobrunov@billing.ru>
2017-12-21 17:21:58 +0500

    init app

---
+++ Remote4Support/Gui/FrmMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Remote4Support.Gui
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Remote4Support/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using Remote4Support.Gui;
+
+namespace Remote4Support
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmMain());
+        }
+    }
+}
