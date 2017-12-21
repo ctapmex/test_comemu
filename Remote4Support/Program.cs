5973cb06d06b9f4967525ba931794af07ec35281
Aleksey Dobrunov <Aleksey.Dobrunov@billing.ru>
2017-12-21 23:39:43 +0500

    enable HighDPI

--- Remote4Support/Program.cs
+++ Remote4Support/Program.cs
@@ -12,9 +12,15 @@ namespace Remote4Support
         [STAThread]
         static void Main()
         {
+            if (Environment.OSVersion.Version.Major >= 6) SetProcessDPIAware();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
         }
+
+        /*for DPI*/
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
 }
